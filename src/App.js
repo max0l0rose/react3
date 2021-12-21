fe6b4cb910e6e466ff1c247e3c34ec684c85e0a5
9956799@gmail.com <Qqq_12345>
2021-12-21 21:58:36 +0200

    AppBar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 import {Main} from "./Comps/Main";
 import {MyAppContextProvider} from "./Comps/AppContext";
 import {Alert} from "./Comps/Alert";
+import {AppBar, Button, IconButton, Toolbar} from "@mui/material";
+import {Menu} from "@mui/icons-material";
 //import {ShoppingList, ShoppingList2} from "./ShoppingList/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <AppBar>
+              <Toolbar>
+                  <IconButton>
+                      <Menu />
+                  </IconButton>
+                  <Button>bbbbb</Button>
+              </Toolbar>
+          </AppBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
